16b989527804211d9c1cf07009aaa14651e848ec
venkat.kamani <venkat16042020@gmail.com>
2024-04-13 02:26:26 +0530

    apk file added.

--- src/mobile_app_tests/login_module_tests.py
+++ src/mobile_app_tests/login_module_tests.py
@@ -3,7 +3,7 @@ from appium import webdriver
 from appium.options.android import UiAutomator2Options
 from appium.webdriver.common.appiumby import AppiumBy
 
-capabilities = dict(
+capabilities1 = dict(
     platformName='Android',
     automationName='uiautomator2',
     deviceName='emulator-5554',
@@ -13,14 +13,30 @@ capabilities = dict(
     locale='US'
 )
 
+capabilities = dict(
+    platformName='Android',
+    automationName='uiautomator2',
+    deviceName='emulator-5554',
+    app=r'C:\Users\venkat.kamani\PycharmProjects\MobileTestAutomatoin\APKs\app-release.apk',
+    language='en',
+    locale='US'
+)
 appium_server_url = 'http://127.0.0.1:4723/wd/hub'
 
 
-def test_login():
+def test_test1():
     options = UiAutomator2Options()
     options.load_capabilities(capabilities)
     driver = webdriver.Remote(appium_server_url, options=options)
     print(driver.page_source)
-    el = driver.find_element(by=AppiumBy.XPATH, value='//*[@text="Battery"]')
+    el = driver.find_element(by=AppiumBy.ID, value='orderBtnOnSignInScrn')
     el.click()
 
+
+def login():
+    options = UiAutomator2Options()
+    options.load_capabilities(capabilities)
+    driver = webdriver.Remote(appium_server_url, options=options)
+    print(driver.page_source)
+    el = driver.find_element(by=AppiumBy.XPATH, value='//*[@text="Place your Order"]')
+    el.click()
